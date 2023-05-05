570c3c9a8c6684ae404b42add7b89ea3eac54f32
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-05-04 23:15:46 -0700

    Componiendo errores

--- Equipo/Complements/Menu.php
+++ Equipo/Complements/Menu.php
@@ -45,7 +45,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="../config/sign-in.php">
+          <a class="nav-link text-white " href="../sign-in.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">login</i>
             </div>
