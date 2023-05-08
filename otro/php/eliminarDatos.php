5f725a905c39d49206171b8cb44bc4864529e8e2
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-05-07 21:11:36 -0700

    Eliminando archivos que considero innecesarios

--- otro/php/eliminarDatos.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-	require_once "conexion.php";
-	$conexion=conexion();
-	$id=$_POST['id'];
-
-	$sql="DELETE from t_persona where id='$id'";
-	echo $result=mysqli_query($conexion,$sql);
- ?>
