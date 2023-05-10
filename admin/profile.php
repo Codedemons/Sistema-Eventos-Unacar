7a2ba5d810cbee60794caba75d0848295583c401
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-05-09 20:34:42 -0700

    Cambio de imagen en la seccion profile de las interfaces

--- admin/profile.php
+++ admin/profile.php
@@ -110,8 +110,8 @@ if(mysqli_num_rows($resultGeneral) == 1){
       </div>
     </nav>
     <!-- End Navbar -->
-    <div class="container-fluid px-2 px-md-4">
-      <div class="page-header min-height-300 border-radius-xl mt-4" style="background-image: url('https://images.unsplash.com/photo-1531512073830-ba890ca4eba2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80');">
+    <div class="container-fluid px-2 px-md-4"> 
+      <div class="page-header min-height-300 border-radius-xl mt-4" style="background-image: url('../assets/img/CTI.png');">
         <span class="mask  bg-gradient-primary  opacity-6"></span>
       </div>
       <div class="card card-body mx-3 mx-md-4 mt-n6">
