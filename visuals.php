7cb2a5d68b80213ca222a9407ebcc13cf9e8f7b1
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-05-15 10:26:04 -0700

    adptacion de los link

--- visuals.php
+++ visuals.php
@@ -1,26 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en"></html>
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta  http-equiv="refresh" content="">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.ico">
-  <link rel="icon" type="image/png" href="assets/img/favicon.ico">
-  <title>
-    Unacar Eventos
-  </title>
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-  <!-- Nucleo Icons -->
-  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <!-- Material Icons -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-  <!-- CSS Files -->
-  <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
+  <?php 
+    require "./config/header.php";
+  ?>
 </head>
 
 <body class="bg-gray-200">
